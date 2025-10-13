6a878ce8ff615dd55d0955cecf723edc86d7561a
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2025-10-13 21:19:47 +0300

    Create a unit conversions array

---
+++ src/components/recipeCOnverter/unitConversions.js
@@ -0,0 +1,42 @@
+export const UNIT_CONVERSIONS = [
+  {
+    unitVariants: ["g", "gram", "grams"],
+    targetUnit: "oz",
+    category: "weight"
+  },
+  {
+    unitVariants: ["kg", "kilogram", "kilograms"],
+    targetUnit: "lb",
+    category: "weight"
+  },
+  {
+    unitVariants: ["oz", "ounce", "ounces"],
+    targetUnit: "g",
+    category: "weight"
+  },
+  {
+    unitVariants: ["lb", "pound", "pounds"],
+    targetUnit: "kg",
+    category: "weight"
+  },
+  {
+    unitVariants: ["dl", "deciliter", "deciliters"],
+    targetUnit: "cup",
+    category: "volume"
+  },
+  {
+    unitVariants: ["c", "cup", "cups"],
+    targetUnit: "dl",
+    category: "volume"
+  },
+  {
+    unitVariants: ["pt", "pint", "pints"],
+    targetUnit: "l",
+    category: "volume"
+  },
+  {
+    unitVariants: ["l", "liter", "liters"],
+    targetUnit: "pt",
+    category: "volume"
+  }
+]
