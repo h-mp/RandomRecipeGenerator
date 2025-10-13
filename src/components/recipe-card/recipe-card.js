7faaa6ba59590b5dd4cc75598cde9b70edaebdd8
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2025-10-13 18:45:24 +0300

    Make functionality for conversion button

--- src/components/recipe-card/recipe-card.js
+++ src/components/recipe-card/recipe-card.js
@@ -7,21 +7,41 @@
 
 import {recipeCardTemplate} from './recipeCardTemplate.js'
 import '../custom-button/custom-button.js'
+import { RecipeConverter } from '../recipeConverter/recipeConverter.js'
 
 customElements.define("recipe-card",
   class RecipeCard extends HTMLElement {
+    /**
+     * The recipe data object.
+     */
+    #recipe
+
+    /**
+     * RecipeConverter instance for converting ingredient amounts.
+     */
+    #recipeConverter
 
     constructor () {
       super()
 
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(recipeCardTemplate.content.cloneNode(true))
+
+      this.#recipeConverter = new RecipeConverter()
     }
 
     connectedCallback () {
       const customButton = document.createElement('custom-button')
       customButton.action = 'convert-values'
-      this.shadowRoot.querySelector('.customButtonContainer').appendChild(customButton)
+      this.shadowRoot.querySelector('.customButtonContainer')
+      .appendChild(customButton)
+
+      customButton.addEventListener('button-click', e => {
+        if (e.detail.action === 'convert-values') {
+          const convertedIngredients = this.#recipeConverter.convertIngredientValues(this.#recipe)
+          this.#displayIngredients(convertedIngredients)
+        }
+      })
     }
 
     /**
@@ -33,7 +53,17 @@ customElements.define("recipe-card",
       if (!recipeData) {
         return
       }
+      this.#recipe = recipeData
 
+      this.#displayRecipe(recipeData)
+    }
+
+    /**
+     * Displays the recipe data in the recipe card.
+     * 
+     * @param {Object} recipeData - The recipe data object.
+     */
+    #displayRecipe (recipeData) {
       const title = this.shadowRoot.querySelector('.recipeTitle')
       const image = this.shadowRoot.querySelector('.recipeImage')
       const origin = this.shadowRoot.querySelector('.recipeOrigin')
